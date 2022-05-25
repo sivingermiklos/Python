358661398f5218ddca3974a85b2734f18f3d2d22
sivingermiklos <sivinger.miklos.martin.2020i@bankitatabanya.hu>
2022-05-25 10:26:34 +0200

    05.25. órai

--- 0525/0525.py
+++ 0525/0525.py
@@ -1,3 +1,5 @@
+from xmlrpc.client import boolean
+
 # OSZTÁLY:
 
 class Haromszog:
@@ -13,11 +15,53 @@ class Haromszog:
         
     # osztály metódusa: szöveggel visszatérve megmondja, hogy a számok háromszöget alkotnak-e    
     def haromszoge(self) -> str:
-        if self.a<self.b+self.c and self.b<self.a+self.c and self.c<self.a+self.b:
+        if self.a < self.b + self.c and self.b < self.a + self.c and self.c < self.a + self.b:
             return "Háromszöget alkotnak"
         else:
             return "Nem alkotnak háromszöget."
-    
+
     # osztály metódusa: egész számként visszaadja a háromszög kerületét
-    def kerulet(self)-> int            :
+    def kerulet(self)-> int:
         return self.a + self.b + self.c
+    
+    def derekszogue(self) -> boolean:
+        return self.a^2 +self.b^2 == self.c^2
+
+    def vanEBenne(self, szam: int) -> boolean:
+        return szam == self.a or szam == self.b or szam == self.c
+
+f = open("haromszog.txt", "r" ,encoding="utf-8")
+
+lista = []
+
+f.readline()
+
+for sor in f:
+    lista.append(sor.strip().split("*"))
+print(lista)
+
+
+for item in lista:
+    print(item)
+    egyHaromszog = Haromszog(item)
+    print(egyHaromszog.haromszoge())
+    print("Háromszög kerülete:",egyHaromszog.kerulet())
+
+print(lista)
+
+# 1. feladat: Kérj be a felhasználótól 3 számot (megfelelő adatszerkezetben),
+# majd írd ki neki, hogy háromszöget alkotnak-e!
+print("\n----------1. feladat----------")
+haromszoglista = []
+
+for i in range(3):
+    szam = int(input("Add meg a háromszög oldalát: "))
+    haromszoglista.append(szam)
+teljesharomszog = Haromszog(haromszoglista)
+print(teljesharomszog.haromszoge())
+print(teljesharomszog.derekszogue())
+
+egySzam = int(input("Adj meg egy számot: "))
+print(teljesharomszog.vanEBenne(egySzam))
+
+
